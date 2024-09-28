834d64d0b803e81df74e015de4bb63b8a4a8bbca
T.B.S <50263561+EasyGoingLove@users.noreply.github.com>
2024-09-28 11:11:50 +0300

    Update CocktailCard.tsx

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -80,6 +80,8 @@ const HeartIcon = styled.div<{ $isFavorite: boolean }>`
   justify-content: center;
   align-items: center;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+  z-index:9999;
+  pointer-events: auto;
 
   svg {
     width: 24px;
@@ -180,6 +182,7 @@ const CocktailCard: React.FC<{
               <HeartIcon
                 $isFavorite={isFavorite}
                 onClick={() => onToggleFavorite(drinkId)}
+                onTouchStart={() => onToggleFavorite(drinkId)}
               >
                 <svg viewBox="0 0 24 24" width="100%" height="100%">
                   <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
